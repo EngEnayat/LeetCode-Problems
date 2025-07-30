039fd9b29b1982ad81c53539f5ce32365d1a01c5
Eng.Enayat <enayatullahbalaghi2@gmail.com>
2025-07-30 23:19:34 +0330

    Intersection of two arrays problem

---
+++ intersectionArrauys.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+        vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    
+            unordered_map<int, int> comp;
+            vector<int> result;
+            for(int it: nums1) comp[it] = 0;
+            for(int j : nums2) 
+            {
+                if(comp.count(j)) comp[j]++;
+            }
+            for(auto it: comp)
+            {
+                if(it.second != 0) result.push_back(it.first);
+            }
+            return result;
+        }
+    };
