dfcddacc6fdf9efa3d2a5407ef8cd9db371a2e83
Eng.Enayat <enayatullahbalaghi2@gmail.com>
2025-07-31 14:52:33 +0330

    Palindrome Linked List problem

---
+++ PalindromeLink.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+        bool isPalindrome(ListNode* head) {
+            stack <int> comp;
+            ListNode *ptr = new ListNode;
+            ptr = head;
+            while (ptr)
+            {
+                comp.push(ptr->val);
+                ptr = ptr->next;
+            }
+            while(head)
+            {
+                if(head->val != comp.top()) {
+                    return false;
+                }
+                head = head->next;
+                comp.pop();
+            }
+            return true;
+        }
+    };
