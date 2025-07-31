685205c18aa53deeeea39b71d99fc10034bac24d
Eng.Enayat <enayatullahbalaghi2@gmail.com>
2025-07-31 23:44:37 +0330

    Updating the Valid Paranthesis with implementation using stack DSA

--- ValidParant.cpp
+++ ValidParant.cpp
@@ -1,40 +1,49 @@
 #include <iostream>
+#include <stack>
 #include <string>
 using namespace std;
-class Bracket
+class Solution
 {
 public:
     bool isValid(string s)
     {
-    int leftBrack, rightBrack;
-    int leftPar, rightPar;      
-    int rightAko, leftAko;      
-    leftBrack= rightBrack= leftPar = rightPar = rightAko= leftAko = 0;
-    int number=0;
-    for(char i : s)
-    {
-        number++;
-        if(i == '(')
+        if(s.size()<=1) return false;
+        if(s[0] == ']' || s[0] == ')' || s[0] == '}') return false;
+        stack<char> par;
+        for(char c: s)
         {
-            if(s[number ] == ']' || s[number] == '}' ) return false;
-            leftPar ++;
-        }  
-        else if(i == ')') rightPar ++;
-        else if(i == '[')
-        {   
-            if(s[number] == ')' || s[number+1] == '}') return false;
-            // cout << "Num: "<< number << s[number] << "i here: "<< i<< endl;
-            leftBrack++;
+            if(c == ')')
+            {
+                if(!par.empty()){
+                    if(par.top() == '(') 
+                    {
+                    par.pop();
+                    continue;
+                    }
+                }
+                par.push(c);
+            }
+            else if(c == ']') 
+            {
+                if(!par.empty()){
+                    if(par.top() == '[') {
+                    par.pop();
+                    continue;
+                    }
+                }
+                par.push(c);
+            }
+            else if( c == '}')
+            {
+                if(!par.empty()){
+                    if(par.top() == '{') 
+                    {par.pop();
+                    continue;}
+                }
+                par.push(c);
+            }
+            else par.push(c);
         }
-        else if(i == ']') rightBrack++;
-        else if(i == '{')
-        {
-            if(s[number ] == ')' || s[number ] == ']') return false;
-            leftAko++;
-        }
-        else if(i == '}') rightAko++;
+        return par.empty();
     }
-    if(leftBrack == rightBrack && leftPar== rightPar && rightAko== leftAko) return true;
-    else return false;
-}
 };
